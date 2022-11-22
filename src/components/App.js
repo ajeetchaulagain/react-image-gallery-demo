8e13f0f8018e148df6b1d2f93845d44611c04c15
ajeetchaulgain <chaulagainajeet@gmail.com>
2022-11-22 17:41:50 +1100

    cleanup, add styles and required component - final

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import ImageGallery from "react-image-gallery";
+import { images } from "../data/gallery-image";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <div className="header-wrapper">
+          <h1>React image gallery demo</h1>
+        </div>
+      </header>
+      <div className="image-gallery-wrapper">
+        <ImageGallery items={images} autoPlay={true} showThumbnails={false} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
